a1fd56ff499d4bbf96aaa0299afb16d02948b96b
Zamier Zaharul <zamier.zaharul@terang.asia>
2019-01-24 09:23:33 +0800

    Added IdentityServiceHelper

--- back/src/test/java/com/assettagging/spotit/helper/IdentityServiceHelper.java
+++ back/src/test/java/com/assettagging/spotit/helper/IdentityServiceHelper.java
@@ -1,5 +1,20 @@
 package com.assettagging.spotit.helper;
 
+import com.assettagging.spotit.identity.business.service.IdentityService;
+import com.assettagging.spotit.identity.domain.model.*;
+import com.assettagging.spotit.security.business.service.SecurityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
 @Service
 public class IdentityServiceHelper {
     private static final Logger log = LoggerFactory.getLogger(IdentityServiceHelper.class);
