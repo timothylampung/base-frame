20fc56cc5f1954cef4d0c2c562a9bfa60b3c8cac
Zamier Zaharul <zamier.zaharul@terang.asia>
2019-01-24 09:19:17 +0800

    Added IdentityServiceHelper

---
+++ back/src/test/java/com/assettagging/spotit/helper/IdentityServiceHelper.java
@@ -0,0 +1,52 @@
+package com.assettagging.spotit.helper;
+
+@Service
+public class IdentityServiceHelper {
+    private static final Logger log = LoggerFactory.getLogger(IdentityServiceHelper.class);
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private IdentityService identityService;
+
+    @Autowired
+    private SecurityService securityService;
+
+    public void changeUser(String username) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, "abc123");
+        Authentication authed = authenticationManager.authenticate(token);
+        SecurityContextHolder.getContext().setAuthentication(authed);
+    }
+
+    public DexUser getCurrentUser() {
+        return securityService.getCurrentUser();
+    }
+
+    public DexUser changeUserByGroup(String groupName) {
+        log.debug("Loading user for {}", groupName);
+        DexGroup group = identityService.findGroupByName(groupName);
+        List<DexPrincipal> members = identityService.findGroupMembers(group);
+        for (DexPrincipal member : members) {
+            if (member.getPrincipalType().equals(DexPrincipalType.USER)) {
+                String username = ((DexUser) member).getUsername();
+                log.debug("Changing user to {}", username);
+                changeUser(username);
+                return (DexUser) member;
+            }
+        }
+        return null;
+    }
+
+    public boolean findUserRole(DexUser user, DexRoleType roleType) {
+        Set<DexGroup> groups = identityService.findEffectiveGroups(user);
+        for (DexGroup group : groups) {
+            for (DexPrincipalRole role : group.getRoles()) {
+                if (role.getRole().equals(roleType)) return true;
+            }
+        }
+        return false;
+
+    }
+
+}
