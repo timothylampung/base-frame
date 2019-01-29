70e7c9eae67e60dbb3750e0ce2046031c6f61bbe
Zamier Zaharul <zamier.zaharul@terang.asia>
2019-01-29 11:51:51 +0800

    Created Asset and Location vo

---
+++ back/src/main/java/com/assettagging/spotit/asset/api/vo/Asset.java
@@ -0,0 +1,26 @@
+package com.assettagging.spotit.asset.api.vo;
+
+import com.assettagging.spotit.asset.domain.model.DexAssetCode;
+import com.assettagging.spotit.asset.domain.model.DexLocation;
+
+public class Asset {
+    private String code;
+    private String description;
+    private DexLocation location;
+    private DexAssetCode assetCode;
+    private Long id;
+
+    public String getCode() { return code; }
+    public void setCode(String code) { this.code = code; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public void setId(Long Id) { this.id = id;}
+
+    public DexLocation getLocation() { return location; }
+    public void setLocation(DexLocation location) { this.location = location; }
+
+    public DexAssetCode getAssetCode() { return assetCode; }
+    public void setAssetCode(DexAssetCode assetCode) { this.assetCode = assetCode; }
+}
---
+++ back/src/main/java/com/assettagging/spotit/asset/api/vo/Location.java
@@ -0,0 +1,38 @@
+package com.assettagging.spotit.asset.api.vo;
+
+import com.assettagging.spotit.asset.domain.model.DexLocation;
+import com.assettagging.spotit.core.api.MetaObject;
+
+public class Location extends MetaObject {
+    private String code;
+    private String description;
+    private Long id;
+    private DexLocation parent;
+    private String address;
+    private String name;
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(Long Id) { this.id = id;}
+
+    public DexLocation getParent() { return parent; }
+    public void setParent(DexLocation parent) { this.parent = parent; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
