655aaea335d426dd998f40b867630c061b746b05
Zamier Zaharul <zamier.zaharul@terang.asia>
2019-02-12 12:32:28 +0800

    added location detail page

---
+++ front/src/app/modules/asset/locations/location-detail/location-detail.page.ts
@@ -0,0 +1,35 @@
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {select, Store} from '@ngrx/store';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AssetState} from "../../asset.state";
+import {Location} from "../location-model";
+
+@Component({
+    selector: 'dex-location-detail-page',
+    templateUrl: './location-detail.page.html'
+})
+export class LocationDetailPage implements OnInit, OnChanges {
+
+    title = 'Locations';
+    @Input() selectedRow: Location;
+    edit: boolean;
+
+    constructor(public fb: FormBuilder,
+                public store: Store<AssetState>,
+                public route: ActivatedRoute,
+                public router: Router) {
+    }
+
+    ngOnInit() {
+
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log(changes);
+        this.edit = this.selectedRow == undefined;
+    }
+
+
+}
+
