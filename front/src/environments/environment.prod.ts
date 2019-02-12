9e416587bd90c549dacbc8ba0c326a1836dd85df
Rafizan Baharum <rafizan.baharum@canang.com.my>
2019-02-12 12:46:25 +0800

    Fix production env

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     appName: 'Sapiei CLIC System',
     production: true,
-    // endpoint: 'http://209.97.162.230:8080/mpsp-personnel',
-    endpoint: 'http://localhost:8080',
+    endpoint: 'http://159.65.3.103:8080/spotit',
 };
