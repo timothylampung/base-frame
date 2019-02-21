7b56207a3b3225549eb734e9faf9017bfd18046e
Zamier Zaharul <zamier.zaharul@terang.asia>
2019-02-21 16:52:04 +0800

    added delete button for location

--- front/src/app/modules/asset/locations/location-list.page.ts
+++ front/src/app/modules/asset/locations/location-list.page.ts
@@ -6,9 +6,9 @@ import {select, Store} from "@ngrx/store";
 import {BreadcrumbService} from "../../../breadcrumb.service";
 import {selectLocationResultState} from "./location-selector";
 import {Observable} from "rxjs";
-import {FindPagedLocationsAction} from "./location-action";
+import {FindPagedLocationsAction, RemoveLocationAction} from "./location-action";
 import {Location, LocationResult} from "./location-model";
-import {FindPagedUsersAction} from "../../identity/principals/user/user.action";
+import {ConfirmationService, Message} from "primeng/api";
 
 @Component({
     selector: 'dex-location-list-page',
@@ -20,6 +20,9 @@ export class LocationListPage implements OnInit {
     searchForm: FormGroup;
     searchQuery : string = '';
     selectedRow : Location = null;
+    display : boolean = false;
+    displayDelete: boolean = false;
+    msgs: Message[] = [];
 
     title = 'Locations';
     cols = [
@@ -30,13 +33,13 @@ export class LocationListPage implements OnInit {
         {label: 'Pengurusan'},
         {label: 'Locations', routerLink: ['/administration/locations/list']}
     ];
-    display : boolean = false;
 
     constructor(public breadcrumbService: BreadcrumbService,
                 public fb: FormBuilder,
                 public store: Store<AssetState>,
                 public route: ActivatedRoute,
-                public router: Router) {
+                public router: Router,
+                public confirmationService: ConfirmationService) {
         this.breadcrumbService.setItems(this.breadcrumbs);
         this.locations$ = this.store.pipe(select(selectLocationResultState));
     }
@@ -47,6 +50,34 @@ export class LocationListPage implements OnInit {
         });
         this.store.dispatch(new FindPagedLocationsAction({filter: '', page: 1}));
         this.locations$.subscribe(data=>{console.log(data)});
+    }
+
+    dialogClosed(result) {
+        console.log(result);
+        this.display = !result;
+    }
+
+    deleteLocation() {
+        this.displayDelete = true;
+    }
+
+    cancelDelete() {
+        this.displayDelete = false;
+    }
+
+    confirmDelete(rowData : Location) {
+        this.confirmationService.confirm({
+            message: 'Are you sure you want to delete this?',
+            header: 'Delete Confirmation',
+            icon: 'pi pi-info-circle',
+            accept: () => {
+                this.msgs = [{severity:'info', summary:'Location Deleted'}];
+                this.store.dispatch(new RemoveLocationAction(rowData));
+            },
+            reject: () => {
+                this.msgs = [{severity:'info', summary:'Delete cancelled'}];
+            }
+        });
     }
 
     search() {
