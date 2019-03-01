df565d9426d1d02d2c47ca8ef8c080adfabf536d
Rafizan Baharum <rafizan.baharum@canang.com.my>
2019-03-01 18:06:53 +0800

    Fix build script

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-figlet "SPOT IT"
+figlet "MPSP TASK"
 
-cd /root/projects/spotit/front
+cd /root/projects/spotit-web/front
 git pull
 echo "DELETING DIST FOLDER"
 rm -rf dist/
-echo "BUILDING SPOTIT FRONTEND"
-ng build -c dev --base-href  /spotit/ --deploy-url /spotit/
-echo "PAYMENT FRONTEND COMPLETED!"
+echo "BUILDING TASK FRONTEND"
+ng build -c dev --base-href  /spotit-web/ --deploy-url /spotit-web/
+echo "TASK FRONTEND COMPLETED!"
 
-cd /root/projects/spotit/back
+cd /root/projects/spotit-web/back
 git pull
 echo "BUILDING WAR FILE"
 mvn -DskipTests=true clean package
 echo "REMOVING OLD WEBAPPS"
-rm -rf /opt/tomcat/webapps/spotit
+rm -rf /opt/tomcat/webapps/spotit-web
 echo "COPYING TARGET WAR TO TOMCAT"
-cp /root/projects/spotit/back/target/spotit.war /opt/tomcat/webapps/
+cp /root/projects/spotit-web/back/target/spotit-web.war /opt/tomcat/webapps/
 echo "COMPLETED!"
