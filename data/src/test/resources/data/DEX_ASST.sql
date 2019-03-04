5b51d7d9290bb7312e3745f261927134614c8aed
Amirul Yunik <amirul.yunik@terang.asia>
2019-03-04 17:06:08 +0800

    Edit Asset Table

--- data/src/test/resources/data/DEX_ASST.sql
+++ data/src/test/resources/data/DEX_ASST.sql
@@ -1,7 +1,9 @@
-INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id) VALUES (1, 'ASST_005', null, 'SINKI', null, null, null, null, null, null, 1, null, 1, 1);
-INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id) VALUES (3, 'ASST_001', null, 'AIRCOND', null, null, null, null, null, null, 1, null, 1, 1);
-INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id) VALUES (4, 'ASST_002', null, 'KIPAS', null, null, null, null, null, null, 1, null, 1, 1);
-INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id) VALUES (5, 'ASST_003', null, 'LAMPU KALIMANTANG', null, null, null, null, null, null, 1, null, 1, 1);
-INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id) VALUES (6, 'ASST_004', null, 'MEJA', null, null, null, null, null, null, 1, null, 1, 1);
-INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id) VALUES (7, 'ASSET_CODE', null, 'ASSET_DESC', '2019-02-07 15:04:01.624000', 1, null, null, null, null, 1, null, 1, 1);
+INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id, category) VALUES (1, 'ASST_005', null, 'SINKI', null, null, null, null, null, null, 1, null, 1, 1, 'Plumbing');
+INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id, category) VALUES (3, 'ASST_001', null, 'AIRCOND', null, null, null, null, null, null, 1, null, 1, 1, 'Electrical');
+INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id, category) VALUES (4, 'ASST_002', null, 'KIPAS', null, null, null, null, null, null, 1, null, 1, 1, 'Electrical');
+INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id, category) VALUES (5, 'ASST_003', null, 'LAMPU KALIMANTANG', null, null, null, null, null, null, 1, null, 1, 1, 'Electrical');
+INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id, category) VALUES (6, 'ASST_004', null, 'MEJA', null, null, null, null, null, null, 1, null, 1, 1, 'Electrical');
+INSERT INTO public.dex_asst (id, code, cost, description, c_ts, c_id, d_ts, d_id, m_ts, m_id, m_st, quantity, asset_code_id, location_id, category) VALUES (7, 'ASSET_CODE', null, 'ASSET_DESC', '2019-02-07 15:04:01.624000', 1, null, null, null, null, 1, null, 1, 1, 'Furniture');
+
+
 
