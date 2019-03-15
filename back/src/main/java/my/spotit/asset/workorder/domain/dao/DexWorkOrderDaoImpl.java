8ee127f95fcf863287bca38fcdb0c11f34c40257
timothylampung <timothy.lampung@canang.com.my>
2019-03-15 12:05:44 +0800

    Reset environment to local

--- back/src/main/java/my/spotit/asset/workorder/domain/dao/DexWorkOrderDaoImpl.java
+++ back/src/main/java/my/spotit/asset/workorder/domain/dao/DexWorkOrderDaoImpl.java
@@ -167,7 +167,7 @@ public class DexWorkOrderDaoImpl extends GenericDaoSupport<Long, DexWorkOrder> i
 
     @Override
     public Integer countActivity(String filter, DexWorkOrder order) {
-        Query query = entityManager.createQuery("select count(s) from DexActivity s where " +
+        Query query = entityManager.createQuery("select count(s) from DexWorkOrderActivity s where " +
                 "upper(s.description) like upper(:filter) " +
                 "and s.workOrder = :order " +
                 "and s.metadata.state = :state ");
@@ -179,7 +179,7 @@ public class DexWorkOrderDaoImpl extends GenericDaoSupport<Long, DexWorkOrder> i
 
     @Override
     public Integer countActivity(DexWorkOrder order) {
-        Query query = entityManager.createQuery("select count(s) from DexActivity s where " +
+        Query query = entityManager.createQuery("select count(s) from DexWorkOrderActivity s where " +
                 "s.workOrder = :order " +
                 "and s.metadata.state = :state ");
         query.setParameter("order", order);
@@ -190,10 +190,8 @@ public class DexWorkOrderDaoImpl extends GenericDaoSupport<Long, DexWorkOrder> i
     @Override
     public Integer countLog(String filter, DexWorkOrder order) {
         Query query = entityManager.createQuery("select count(s) from DexWorkOrderLog s where " +
-                "upper(s.description) like upper(:filter) " +
-                "and s.workOrder = :order " +
+                "s.workOrder = :order " +
                 "and s.metadata.state = :state ");
-        query.setParameter("filter", WILDCARD + filter + WILDCARD);
         query.setParameter("order", order);
         query.setParameter("state", DexMetaState.ACTIVE);
         return ((Long) query.getSingleResult()).intValue();
