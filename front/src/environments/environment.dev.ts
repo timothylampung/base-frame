d14a50f91eb26cb7c0c1038dceea9c3bc99fabd1
Rafizan Baharum <rafizan.baharum@canang.com.my>
2019-03-19 12:46:14 +0800

    Fix production config

--- front/src/environments/environment.dev.ts
+++ front/src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     appName: 'SPOT IT',
     production: true,
-    endpoint: 'http://159.65.3.103:8080/spotit-web',
+    endpoint: 'http://159.65.3.103:8080/spotit',
     // endpoint: 'http://localhost:8080',
 };
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     appName: 'SPOT IT',
     production: false,
-    // endpoint: 'http://159.65.3.103:8080/spotit-web',
-    endpoint: 'http://localhost:8080',
+    endpoint: 'http://159.65.3.103:8080/spotit',
+    // endpoint: 'http://localhost:8080',
 };
