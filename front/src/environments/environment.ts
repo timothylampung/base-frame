3a92944c772db7794e4b9d0fb80bddd4aeacf0b5
timothylampung <timothy.lampung@canang.com.my>
2019-03-22 15:16:41 +0800

    waalkthrough push

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,8 +1,9 @@
 export const environment = {
     appName: 'SPOT IT',
     production: false,
-// endpoint: 'http://159.65.3.103:8080/spotit',
-    // wsEndpoint: 'ws://159.65.3.103:8080/spotit',
-    endpoint: 'http://localhost:8080/spotit',
-    wsEndpoint: 'ws://localhost:8080/spotit',
+    endpoint: 'http://159.65.3.103:8080/spotit',
+    wsEndpoint: 'ws://159.65.3.103:8080/spotit',
+    // endpoint: 'http://localhost:8080/spotit',
+    // wsEndpoint: 'ws://localhost:8080/spotit',
+
 };
